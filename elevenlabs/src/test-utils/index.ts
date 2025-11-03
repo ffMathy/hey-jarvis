2a41a66ec47ce1a4ffeb245cce933fc943e63bf4
ffMathy <mathias.lorenzen@live.com>
2025-11-03 18:18:40 +0000

    fix: refactor to use strategy pattern

--- elevenlabs/src/test-utils/index.ts
+++ elevenlabs/src/test-utils/index.ts
@@ -1,5 +1,12 @@
 export {
   TestConversation,
   type ConversationOptions,
+  type ConversationStrategy,
   type EvaluationResult,
 } from './test-conversation';
+
+export {
+  ElevenLabsConversationStrategy,
+  type ElevenLabsConversationOptions,
+  type ServerMessage,
+} from './elevenlabs-conversation-strategy';
